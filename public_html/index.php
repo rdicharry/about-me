1620f2a985ec55a82d0733b71dc57123e4a96ad8
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-13 13:45:32 -0600

    added stylesheet

---
+++ public_html/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+	<html>
+	<head lang="en">
+		<meta charset="UTF-8"/>
+		<!-- add sytlesheet -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+		<title>Rebecca Dicharry</title>
+	</head>
+	<body>
+		<header>
+			<h1 class="mainHeader">Rebecca Dicharry - About Me</h1>
+		</header>
+		<main>
+			<div class="photo">
+				<b>I like hiking.</b><br/>
+				<img src="img/rebecca2015.JPG" alt="Rebecca">
+			</div>
+			<div class="photo">
+				<b>My daily dose of furry is my dog Cookie.</b><br/>
+				<img src="img/cookie.jpg" alt="Cookie Dog"/>
+			</div>
+			<div class="photo">
+				<b>Nature lover.</b> <br/>
+				<img src="img/juniper.JPG" alt="Juniper"/>
+			</div>
+			<section><b>Background:</b>I was born in Las Cruces, and came to Albuquerque after a short residence in Connecticut for graduate school. I have a great love for all things mathematical. I have a degree in chemical engineering, and way too many computer science and math classes under my belt. I am not an artist, but I have much love and appreciation for art and good design. I love getting away from the city for a great hike or camping trip. I love creating and learning how things are constructed.
+
+			</section>
+			<section><b>Things I like:</b>
+				<ul>
+					<li></li>
+					<li>Team coffee or team tea? Both!</li>
+				</ul>
+
+			</section>
+			<section><b>Photos:</b>
+
+			</section>
+		</main>
+	</body>
+</html>
