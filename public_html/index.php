8a167bd740f190e5c2c9672258c2d98efacf80a2
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-13 16:00:26 -0600

    stylesheet adding style, now!

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8"/>
 		<!-- add sytlesheet -->
 		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+		<link href="https://fonts.googleapis.com/css?family=Voltaire" rel="stylesheet">
 		<title>Rebecca Dicharry</title>
 	</head>
 	<body>
@@ -23,19 +24,23 @@
 				<b>Nature lover.</b> <br/>
 				<img src="img/juniper.JPG" alt="Juniper"/>
 			</div>
-			<section><b>Background:</b>I was born in Las Cruces, and came to Albuquerque after a short residence in Connecticut for graduate school. I have a great love for all things mathematical. I have a degree in chemical engineering, and way too many computer science and math classes under my belt. I am not an artist, but I have much love and appreciation for art and good design. I love getting away from the city for a great hike or camping trip. I love creating and learning how things are constructed.
+			<section class="contentItem"><b>Background:</b>I was born in Las Cruces, and came to Albuquerque after a short residence in Connecticut for graduate school. I have a great love for all things mathematical. I have a degree in chemical engineering, and way too many computer science and math classes under my belt. I am not an artist, but I have much love and appreciation for art and good design. I love getting away from the city for a great hike or camping trip. I love creating and learning how things are constructed.
 
 			</section>
-			<section><b>Things I like:</b>
+			<section class="contentItem"><b>Things I like:</b>
 				<ul>
 					<li></li>
 					<li>Team coffee or team tea? Both!</li>
 				</ul>
 
 			</section>
-			<section><b>Photos:</b>
 
+			<section class="contentItem"><b>Favorite Pages:</b>
+				<ul>
+					<li></li>
+				</ul>
 			</section>
+
 		</main>
 	</body>
 </html>
